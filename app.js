a65a2fb6c7fdbcbd5211c70a8376b8b45ad9e27b
Fabio <andresviveros1984@googlemail.com>
2022-05-27 10:37:26 +0100

    added more images to slide show

--- app.js
+++ app.js
@@ -2,14 +2,49 @@ const slideShowSection = document.querySelector('.slide-show-area img');
 
 const imageObArray = [
   {
-    image: 'lady-with-drink.jpeg'
+    image: 'IMG-20220401-WA0026.jpg'
   },
   {
     image: 'lowerdeckwithsmoke.jpeg'
   },
   {
+    image: 'WhatsApp Image 2022-03-14 at 22.35.19 (1).jpeg'
+  },
+  {
     image: 'top-deck.jpeg'
+  },
+  {
+    image:'IMG-20220401-WA0025.jpg'
+  },
+  
+  {
+    image: 'IMG-20220401-WA0027.jpg'
+  },
+  {
+    image: 'IMG-20220401-WA0028.jpg'
+  },
+  {
+    image: 'IMG-20220401-WA0029.jpg'
+  },
+  {
+    image: 'IMG-20220401-WA0030.jpg'
+  },
+  {
+    image: 'IMG-20220401-WA0031.jpg'
+  },
+  {
+    image: 'IMG-20220401-WA0032.jpg'
+  },
+  {
+    image: 'IMG-20220401-WA0033.jpg'
+  },
+  {
+    image: 'IMG-20220401-WA0034.jpg'
+  },
+  {
+    image: 'IMG-20220401-WA0035.jpg'
   }
+  
 ]
 
 
@@ -17,7 +52,7 @@ let count = 0;
 
 const slideShow = () => {
   
-  if(count >= 3){
+  if(count >= imageObArray.length){
     count = 0;
   }
   slideShowSection.src  = `./images/${imageObArray[count].image}`; 
